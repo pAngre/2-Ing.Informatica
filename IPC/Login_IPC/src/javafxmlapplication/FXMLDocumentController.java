5a7fb5d267d7a60080a3967633d3f3959a25ca03
Pablo Angresola <angre@pop-os.localdomain>
2023-02-16 16:25:53 +0100

    IPC

--- IPC/Login_IPC/src/javafxmlapplication/FXMLDocumentController.java
+++ IPC/Login_IPC/src/javafxmlapplication/FXMLDocumentController.java
@@ -30,8 +30,7 @@ public class FXMLDocumentController implements Initializable {
     //                      when the button has the focus and enter is pressed
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        labelMessage.setText("SI");
-        buttonClick.setText("Marc es maricon?");
+        labelMessage.setText("Hello, this is your first JavaFX project - IPC");
     }
     
     //=========================================================
