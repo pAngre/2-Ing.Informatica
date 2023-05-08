cc375a0647fb81ab0f0478276a1642565cb70709
pAngre <pabloangre@gmail.com>
2023-05-08 13:15:04 +0200

    UPV

--- IPC/IPC_FXML_TableView/src/controlador/VistaPersonaController.java
+++ IPC/IPC_FXML_TableView/src/controlador/VistaPersonaController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import modelo.Persona;
 
@@ -27,6 +28,8 @@ public class VistaPersonaController implements Initializable {
     
     private Persona persona;
     private boolean pulsadoOK = false;
+    @FXML
+    private Button aceptarButton;
     
     public void initPersona(Persona p){
         persona = p;
